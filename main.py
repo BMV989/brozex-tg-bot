9d1522d6cc766931f408d2bfc059dfd5506b0a02
Maxim <mbrozovskii@gmail.com>
2024-08-01 17:28:17 +0500

    extract dp and bot setup in a separate function

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from aiogram import Bot, Dispatcher
 from handlers.user_router import rt as user_router
 
 
+def bot_and_dp_setup() -> tuple[Bot, Dispatcher]:
+    bot = Bot(token=cfg.BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(user_router)
+    return bot, dp
+
+
 def webhook_setup() -> None:
     pass
 
 
 async def log_polling_setup() -> None:
-    bot = Bot(token=cfg.BOT_TOKEN)
-    dp = Dispatcher()
-    dp.include_router(user_router)
+    bot, dp = bot_and_dp_setup()
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
